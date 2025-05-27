d09d1087ccf9ea2f308adfce64be036aca581a48
Nedim Hozic <nedimhozic@gmail.com>
2025-05-27 09:00:32 +0200

    Added keyvault

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
+import * as keyvault from "@pulumi/azure-native/keyvault";
 
 const config = new pulumi.Config();
 const environment = config.require("env");
 const storageAccountTier = config.require("storageAccountTier");
+const tenantId = config.require("tenantId");
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup", {
@@ -19,4 +21,29 @@ const storageAccount = new storage.StorageAccount("sa", {
         name: storageAccountTier,
     },
     kind: storage.Kind.StorageV2,
+});
+
+const keyVault = new keyvault.Vault("keyVault", {
+    properties: {
+        enableRbacAuthorization: true,
+        enableSoftDelete: true,
+        enabledForDeployment: false,
+        enabledForDiskEncryption: false,
+        enabledForTemplateDeployment: false,
+        networkAcls: {
+            bypass: keyvault.NetworkRuleBypassOptions.None,
+            defaultAction: keyvault.NetworkRuleAction.Allow,
+        },
+        publicNetworkAccess: "Enabled",
+        sku: {
+            family: keyvault.SkuFamily.A,
+            name: keyvault.SkuName["Standard"],
+        },
+        softDeleteRetentionInDays: 90,
+        tenantId,
+    },
+    resourceGroupName: resourceGroup.name,
+    vaultName: `kv-pulumi-test-${environment}`
+}, {
+    protect: true,
 });
